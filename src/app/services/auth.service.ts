bfde06e68584536d9f75ebc958b7af63ab07fdbe
alisonMSalas <asalas2854@uta.edu.ec>
2025-05-03 00:55:37 -0500

    cambio de puerto

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:9090/api';
+  private apiUrl = 'http://localhost:8080/api';
   constructor(private http: HttpClient) {}
 
   login(credentials: { email: string; password: string }): Observable<string> {
