3388d4a338bf93b754d83e0b6be5cb61e041ea94
Rafael Soriano <raansopro324@gmail.com>
2025-05-06 00:12:46 -0500

    Suscripcion

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { ApiClientService } from '../shared/api/httpclient';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class TransactionService {
-
-  private transactionsUpdated = new BehaviorSubject<void>(undefined);
+  private transactionsUpdated = new Subject<void>();
   transactionsUpdated$ = this.transactionsUpdated.asObservable();
 
-  constructor(private api:ApiClientService) { 
-    
-  }
+  constructor(private api: ApiClientService) { }
 
   create(transaction: any) {
     return this.api.post('transaction', transaction).pipe(
@@ -23,16 +17,18 @@ export class TransactionService {
   }
 
   getAll() {
-    return this.api.get<any[]>('transaction')
+    return this.api.get<any[]>('transaction');
   }
 
   update(transaction: any) {
-    return this.api.put('transaction', transaction)
+    return this.api.put('transaction', transaction).pipe(
+      tap(() => this.transactionsUpdated.next())
+    );
   }
 
   delete(id: number) {
-    return this.api.delete(`transaction?transactionId=${id}`)
+    return this.api.delete(`transaction?transactionId=${id}`).pipe(
+      tap(() => this.transactionsUpdated.next())
+    );
   }
-  
-  
 }
